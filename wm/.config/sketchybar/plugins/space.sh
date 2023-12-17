8d46047ede5571da5e39355fc895fc70578a5af1
junikimm717 <68165832+junikimm717@users.noreply.github.com>
2023-12-17 18:08:46 -0500

    sketchybar updated for mbp

--- wm/.config/sketchybar/plugins/space.sh
+++ wm/.config/sketchybar/plugins/space.sh
@@ -4,4 +4,9 @@
 # the space invoking this script (with name: $NAME) is currently selected:
 # https://felixkratz.github.io/SketchyBar/config/components#space----associate-mission-control-spaces-with-an-item
 
-sketchybar --set $NAME background.drawing=$SELECTED
+if test "$SELECTED" = "true"; then
+sketchybar --set $NAME background.drawing="$SELECTED" highlight_color=0xfff38ba8 icon.color=0xfff38ba8
+else
+sketchybar --set $NAME background.drawing="$SELECTED" highlight_color=0xffffffff icon.color=0xffffffff
+fi
+
--- wm/.config/sketchybar/plugins/wifi.sh
+++ wm/.config/sketchybar/plugins/wifi.sh
@@ -3,6 +3,7 @@
 # The wifi_change event supplies a $INFO variable in which the current SSID
 # is passed to the script.
 
+INFO="$(/System/Library/PrivateFrameworks/Apple80211.framework/Resources/airport -I  | awk -F' SSID: '  '/ SSID: / {print $2}')"
 WIFI=${INFO:-"Not Connected"}
 
 sketchybar --set $NAME label="${WIFI}"
